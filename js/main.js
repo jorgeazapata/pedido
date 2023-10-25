54b4ed1fedb51c1e8f518d072c13de3e4e78b6e7
jorgeazapata <jorgeandresz8910@hotmail.com>
2023-10-25 02:15:04 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+function mostrar(valor){
+    var sttabla=25;
+    document.getElementById("resultado").innerHTML=sttabla-valor;
+    
+}
+function mostrar2(valor){
+    var stmilares=50;
+    document.getElementById("resultado2").innerHTML=stmilares-valor;
+    
+}
+function mostrar3(valor){
+    var stmilares=25;
+    document.getElementById("resultado3").innerHTML=stmilares-valor;
+    
+}
