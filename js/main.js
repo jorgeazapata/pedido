c7e63b1f264828a0d0d96a5e01f170ceea2bf58e
jorgeazapata <jorgeandresz8910@hotmail.com>
2023-10-30 03:35:35 -0500

    portafolio 2.0

--- js/main.js
+++ js/main.js
@@ -18,6 +18,51 @@ function mostrar3(valor){
     document.getElementById("resultado3").innerHTML=stmilares-valor;
     
 }
+function mostrar4(valor){
+    var sttabla=25;
+    document.getElementById("resultado4").innerHTML=sttabla-valor;
+    
+}
+function mostrar5(valor){
+    var stmilares=25;
+    document.getElementById("resultado5").innerHTML=stmilares-valor;
+    
+}
+function mostrar6(valor){
+    var stmilares=25;
+    document.getElementById("resultado6").innerHTML=stmilares-valor;
+    
+}
+function mostrar7(valor){
+    var sttabla=35;
+    document.getElementById("resultado7").innerHTML=sttabla-valor;
+    
+}
+function mostrar8(valor){
+    var stmilares=21;
+    document.getElementById("resultado8").innerHTML=stmilares-valor;
+    
+}
+function mostrar9(valor){
+    var stmilares=40;
+    document.getElementById("resultado9").innerHTML=stmilares-valor;
+    
+}
+function mostrar10(valor){
+    var sttabla=30;
+    document.getElementById("resultado10").innerHTML=sttabla-valor;
+    
+}
+function mostrar11(valor){
+    var stmilares=40;
+    document.getElementById("resultado11").innerHTML=stmilares-valor;
+    
+}
+function mostrar12(valor){
+    var stmilares=25;
+    document.getElementById("resultado12").innerHTML=stmilares-valor;
+    
+}
 function ImprimirPagina(){
     window.print();
 }
