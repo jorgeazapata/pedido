e265f0e8af833006e7ab8f57a932278f8be0eaba
jorgeazapata <jorgeandresz8910@hotmail.com>
2024-01-02 01:53:58 -0500

    seleccion de almacenes y seccion

--- js/fredonia.js
+++ js/fredonia.js
@@ -4,27 +4,27 @@ function inicio(){
     alert("Bienvenido, en esta plataforma deberas ingresar las cantidades reales de cada producto y ésta te entregará un sugerido de pedido.");
 }
 function mostrar(valor){
-    var sttabla=25;
+    var sttabla=40;
     document.getElementById("resultado").innerHTML=sttabla-valor;
     
 }
 function mostrar2(valor){
-    var stmilares=50;
+    var stmilares=100;
     document.getElementById("resultado2").innerHTML=stmilares-valor;
     
 }
 function mostrar3(valor){
-    var stposta=25;
+    var stposta=35;
     document.getElementById("resultado3").innerHTML=stposta-valor;
     
 }
 function mostrar4(valor){
-    var stpecho=25;
+    var stpecho=35;
     document.getElementById("resultado4").innerHTML=stpecho-valor;
     
 }
 function mostrar5(valor){
-    var stmorrillo=25;
+    var stmorrillo=35;
     document.getElementById("resultado5").innerHTML=stmorrillo-valor;
     
 }
@@ -34,32 +34,32 @@ function mostrar6(valor){
     
 }
 function mostrar7(valor){
-    var sthigado=35;
+    var sthigado=50;
     document.getElementById("resultado7").innerHTML=sthigado-valor;
     
 }
 function mostrar8(valor){
-    var stboffe=21;
+    var stboffe=25;
     document.getElementById("resultado8").innerHTML=stboffe-valor;
     
 }
 function mostrar9(valor){
-    var stchuleta=40;
+    var stchuleta=100;
     document.getElementById("resultado9").innerHTML=stchuleta-valor;
     
 }
 function mostrar10(valor){
-    var stmilacerdo=30;
+    var stmilacerdo=80;
     document.getElementById("resultado10").innerHTML=stmilacerdo-valor;
     
 }
 function mostrar11(valor){
-    var steconomica=40;
+    var steconomica=80;
     document.getElementById("resultado11").innerHTML=steconomica-valor;
     
 }
 function mostrar12(valor){
-    var stcannon=25;
+    var stcannon=40;
     document.getElementById("resultado12").innerHTML=stcannon-valor;
     
 }
