262384851598d63f649caaacbca465812ca3e8d4
jorgeazapata <jorgeandresz8910@hotmail.com>
2024-01-14 01:58:41 -0500

    mejora logica

--- js/amaga.js
+++ js/amaga.js
@@ -5,129 +5,62 @@ function inicio(){
 }
 function mostrar(valor){
     var sttabla=40;
-    document.getElementById("resultado").innerHTML=sttabla-valor;window.onload = inicio;
-
-function inicio(){
-    alert("Bienvenido, en esta plataforma deberas ingresar las cantidades reales de cada producto y ésta te entregará un sugerido de pedido.");
-}
-function mostrar(valor){
-    var sttabla=40;
     document.getElementById("resultado").innerHTML=sttabla-valor;
     
 }
-function mostrar2(valor){
-    var stmilares=100;
-    document.getElementById("resultado2").innerHTML=stmilares-valor;
+function mostrar2(valor2){
+    var stmilares=140;
+    document.getElementById("resultado2").innerHTML=stmilares-valor2;
     
 }
-function mostrar3(valor){
-    var stposta=35;
-    document.getElementById("resultado3").innerHTML=stposta-valor;
+function mostrar3(valor3){
+    var stposta=25;
+    document.getElementById("resultado3").innerHTML=stposta-valor3;
     
 }
-function mostrar4(valor){
-    var stpecho=35;
-    document.getElementById("resultado4").innerHTML=stpecho-valor;
+function mostrar4(valor4){
+    var stpecho=25;
+    document.getElementById("resultado4").innerHTML=stpecho-valor4;
     
 }
-function mostrar5(valor){
-    var stmorrillo=35;
-    document.getElementById("resultado5").innerHTML=stmorrillo-valor;
+function mostrar5(valor5){
+    var stmorrillo=40;
+    document.getElementById("resultado5").innerHTML=stmorrillo-valor5;
     
 }
-function mostrar6(valor){
+function mostrar6(valor6){
     var sttableado=25;
-    document.getElementById("resultado6").innerHTML=sttableado-valor;
+    document.getElementById("resultado6").innerHTML=sttableado-valor6;
     
 }
-function mostrar7(valor){
-    var sthigado=50;
-    document.getElementById("resultado7").innerHTML=sthigado-valor;
+function mostrar7(valor7){
+    var sthigado=40;
+    document.getElementById("resultado7").innerHTML=sthigado-valor7;
     
 }
-function mostrar8(valor){
-    var stboffe=25;
-    document.getElementById("resultado8").innerHTML=stboffe-valor;
+function mostrar8(valor8){
+    var stboffe=15;
+    document.getElementById("resultado8").innerHTML=stboffe-valor8;
     
 }
-function mostrar9(valor){
-    var stchuleta=100;
-    document.getElementById("resultado9").innerHTML=stchuleta-valor;
+function mostrar9(valor9){
+    var stchuleta=150;
+    document.getElementById("resultado9").innerHTML=stchuleta-valor9;
     
 }
-function mostrar10(valor){
-    var stmilacerdo=80;
-    document.getElementById("resultado10").innerHTML=stmilacerdo-valor;
+function mostrar10(valor10){
+    var stmilacerdo=120;
+    document.getElementById("resultado10").innerHTML=stmilacerdo-valor10;
     
 }
-function mostrar11(valor){
-    var steconomica=80;
-    document.getElementById("resultado11").innerHTML=steconomica-valor;
+function mostrar11(valor11){
+    var steconomica=140;
+    document.getElementById("resultado11").innerHTML=steconomica-valor11;
     
 }
-function mostrar12(valor){
-    var stcannon=40;
-    document.getElementById("resultado12").innerHTML=stcannon-valor;
-    
-}
-function ImprimirPagina(){
-    window.print();
-}
-    
-}
-function mostrar2(valor){
-    var stmilares=100;
-    document.getElementById("resultado2").innerHTML=stmilares-valor;
-    
-}
-function mostrar3(valor){
-    var stposta=35;
-    document.getElementById("resultado3").innerHTML=stposta-valor;
-    
-}
-function mostrar4(valor){
-    var stpecho=35;
-    document.getElementById("resultado4").innerHTML=stpecho-valor;
-    
-}
-function mostrar5(valor){
-    var stmorrillo=35;
-    document.getElementById("resultado5").innerHTML=stmorrillo-valor;
-    
-}
-function mostrar6(valor){
-    var sttableado=25;
-    document.getElementById("resultado6").innerHTML=sttableado-valor;
-    
-}
-function mostrar7(valor){
-    var sthigado=50;
-    document.getElementById("resultado7").innerHTML=sthigado-valor;
-    
-}
-function mostrar8(valor){
-    var stboffe=25;
-    document.getElementById("resultado8").innerHTML=stboffe-valor;
-    
-}
-function mostrar9(valor){
-    var stchuleta=100;
-    document.getElementById("resultado9").innerHTML=stchuleta-valor;
-    
-}
-function mostrar10(valor){
-    var stmilacerdo=80;
-    document.getElementById("resultado10").innerHTML=stmilacerdo-valor;
-    
-}
-function mostrar11(valor){
-    var steconomica=80;
-    document.getElementById("resultado11").innerHTML=steconomica-valor;
-    
-}
-function mostrar12(valor){
-    var stcannon=40;
-    document.getElementById("resultado12").innerHTML=stcannon-valor;
+function mostrar12(valor12){
+    var stcannon=60;
+    document.getElementById("resultado12").innerHTML=stcannon-valor12;
     
 }
 function ImprimirPagina(){
--- js/main.js
+++ js/main.js
@@ -8,59 +8,59 @@ function mostrar(valor){
     document.getElementById("resultado").innerHTML=sttabla-valor;
     
 }
-function mostrar2(valor){
+function mostrar2(valor2){
     var stmilares=50;
-    document.getElementById("resultado2").innerHTML=stmilares-valor;
+    document.getElementById("resultado2").innerHTML=stmilares-valor2;
     
 }
-function mostrar3(valor){
+function mostrar3(valor3){
     var stposta=25;
-    document.getElementById("resultado3").innerHTML=stposta-valor;
+    document.getElementById("resultado3").innerHTML=stposta-valor3;
     
 }
-function mostrar4(valor){
+function mostrar4(valor4){
     var stpecho=25;
-    document.getElementById("resultado4").innerHTML=stpecho-valor;
+    document.getElementById("resultado4").innerHTML=stpecho-valor4;
     
 }
-function mostrar5(valor){
+function mostrar5(valor5){
     var stmorrillo=25;
-    document.getElementById("resultado5").innerHTML=stmorrillo-valor;
+    document.getElementById("resultado5").innerHTML=stmorrillo-valor5;
     
 }
-function mostrar6(valor){
+function mostrar6(valor6){
     var sttableado=25;
-    document.getElementById("resultado6").innerHTML=sttableado-valor;
+    document.getElementById("resultado6").innerHTML=sttableado-valor6;
     
 }
-function mostrar7(valor){
+function mostrar7(valor7){
     var sthigado=35;
-    document.getElementById("resultado7").innerHTML=sthigado-valor;
+    document.getElementById("resultado7").innerHTML=sthigado-valor7;
     
 }
-function mostrar8(valor){
+function mostrar8(valor8){
     var stboffe=21;
-    document.getElementById("resultado8").innerHTML=stboffe-valor;
+    document.getElementById("resultado8").innerHTML=stboffe-valor8;
     
 }
-function mostrar9(valor){
+function mostrar9(valor9){
     var stchuleta=40;
-    document.getElementById("resultado9").innerHTML=stchuleta-valor;
+    document.getElementById("resultado9").innerHTML=stchuleta-valor9;
     
 }
-function mostrar10(valor){
+function mostrar10(valor10){
     var stmilacerdo=30;
     document.getElementById("resultado10").innerHTML=stmilacerdo-valor;
     
 }
-function mostrar11(valor){
+function mostrar11(valor11){
     var steconomica=40;
-    document.getElementById("resultado11").innerHTML=steconomica-valor;
+    document.getElementById("resultado11").innerHTML=steconomica-valor11;
     
 }
-function mostrar12(valor){
+function mostrar12(valor12){
     var stcannon=25;
-    document.getElementById("resultado12").innerHTML=stcannon-valor;
+    document.getElementById("resultado12").innerHTML=stcannon-valor12;
     
 }
 function ImprimirPagina(){
